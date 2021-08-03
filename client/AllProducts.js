ac5cfa2e4691bd1a433995c3ff9f8dc914b90154
ChrisNieves <nievescs20@gmail.com>
2021-08-03 16:55:38 -0400

    update seed to include product images

--- client/AllProducts.js
+++ client/AllProducts.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
-import {connect} from 'react-redux'
-import {withRouter, Route, Switch, Redirect, Link } from 'react-router-dom'
+import { connect } from 'react-redux';
+import { withRouter, Route, Switch, Redirect, Link } from 'react-router-dom';
 import SingleProduct from './SingleProduct';
 import allProductsReducer, { fetchProducts } from './store/allProductsReducer';
-
 
 class AllProducts extends Component {
   componentDidMount() {
@@ -16,24 +15,26 @@ class AllProducts extends Component {
     return (
       <div>
         <h1>This is the AllProducts component</h1>
-        {this.props.products.map(product =>
-        <div key={product.id}>
-          <div><img src={product.imageURL} alt="picture of soda" /></div>
-        <h3>{product.name}</h3>
-        <h1>{product.price}</h1>
-        </div>
-        )}
+        {this.props.products.map((product) => (
+          <div key={product.id}>
+            <div>
+              <img src={product.imageURL} alt="picture of soda" />
+            </div>
+            <h3>{product.name}</h3>
+            <h1>{product.price}</h1>
+          </div>
+        ))}
       </div>
-    )
+    );
   }
 }
 
 const mapState = (state) => ({
-  products: state.allProductsReducer.products
+  products: state.allProductsReducer.products,
 });
 
 const mapDispatch = (dispatch) => ({
-  getProducts: () => dispatch(fetchProducts())
+  getProducts: () => dispatch(fetchProducts()),
 });
 
 export default withRouter(connect(mapState, mapDispatch)(AllProducts));
