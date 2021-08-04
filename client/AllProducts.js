4fab2c3ac45d1edf87ff2eec7311f064a7909f50
ChrisNieves <nievescs20@gmail.com>
2021-08-04 12:25:08 -0400

    close #3, implemented singleproduct view, route, store, reducer

--- client/AllProducts.js
+++ client/AllProducts.js
@@ -17,11 +17,13 @@ class AllProducts extends Component {
         <h1>This is the AllProducts component</h1>
         {this.props.products.map((product) => (
           <div key={product.id}>
-            <div>
-              <img src={product.imageURL} alt="picture of soda" />
-            </div>
-            <h3>{product.name}</h3>
-            <h1>{product.price}</h1>
+            <Link to={`/products/${product.id}`}>
+              <div>
+                <img src={product.imageURL} alt="picture of soda" />
+              </div>
+              <h3>{product.name}</h3>
+              <h1>${product.price}</h1>
+            </Link>
           </div>
         ))}
       </div>
