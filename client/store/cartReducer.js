abd4755ae1c0610dba25c569a862d84943cee237
ChrisNieves <nievescs20@gmail.com>
2021-08-05 16:33:45 -0400

    all files

--- client/store/cartReducer.js
+++ client/store/cartReducer.js
@@ -4,9 +4,9 @@ import { Order } from '../../server/db';
 const UPDATE_CART = 'UPDATE_CART';
 const GET_CART = 'GET_CART';
 
-const updateCart = (user) => ({
+const updateCart = (cart) => ({
   type: UPDATE_CART,
-  user,
+  cart,
 });
 const getCart = (cart) => ({
   type: GET_CART,
@@ -28,10 +28,10 @@ export const updateCartThunk = (user, product, quantityChange) => {
     try {
       console.log('this is the user in the thunk', user);
       if (!Object.keys(user).length) {
-        console.log('Hello from the if block');
-        window.localStorage.setItem('cart', 'hello world');
-        const msg = window.localStorage.getItem('cart');
-        console.log(msg);
+        const order = Order.build({ quantity: 1 });
+        window.localStorage.setItem('cart', order);
+        const cart = window.localStorage.getItem('cart');
+        dispatch(updateCart(cart));
       } else {
         const { data: updatedUser } = await axios.put(`/api/users/${user.id}`, {
           product: product,
@@ -48,7 +48,7 @@ export const updateCartThunk = (user, product, quantityChange) => {
 export default function cartReducer(state = {}, action) {
   switch (action.type) {
     case UPDATE_CART: {
-      return action.user;
+      return action.cart;
     }
     case GET_CART: {
       return action.cart;
